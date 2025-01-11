a248f16309f4a8d00a041486af411da31e201f4b
Béla Jash <abelxotessema@gmail.com>
2025-01-11 10:11:06 +0300

    Touchables.

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import {StatusBar} from 'expo-status-bar';
-import {SafeAreaView, StyleSheet, Text, Image} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight, TouchableNativeFeedback, View
+} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       {/*<Image source={require('./assets/icon.png')}/>*/}
-      <Image
-        blurRadius={5}
-        loadingIndicatorSource={require('./assets/favicon.png')}
-        fadeDuration={1000} // Only for Android
-        resizeMode="cover"
-        onLoadStart={() => console.log('start')}
-        onLoad={() => console.log('loaded')}
-        onLoadEnd={() => console.log('end')}
-        onError={() => console.log('error')}
-        source={{width: 200, height: 300, uri: 'https://picsum.photos/200/300',}}/>
-      <StatusBar style="auto"/>
+      {/*<TouchableWithoutFeedback onPress={() => console.log('tap')}>*/}
+      {/*<TouchableOpacity onPress={() => console.log('tap')}> /!* For some reason, it only works for double taps only. *!/*/}
+      <TouchableHighlight onPress={() => console.log('tap')}>
+        <Image
+          blurRadius={5}
+          loadingIndicatorSource={require('./assets/favicon.png')}
+          fadeDuration={1000} // Only for Android
+          resizeMode="cover"
+          onLoadStart={() => console.log('start')}
+          onLoad={() => console.log('loaded')}
+          onLoadEnd={() => console.log('end')}
+          onError={() => console.log('error')}
+          source={{width: 200, height: 300, uri: 'https://picsum.photos/200/300',}}/>
+      </TouchableHighlight>
+      <TouchableNativeFeedback>
+        <View style={{width: 200, height: 70, backgroundColor: 'dodgerblue'}}/>
+      </TouchableNativeFeedback>
     </SafeAreaView>
   );
 }
@@ -24,7 +38,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
   },
