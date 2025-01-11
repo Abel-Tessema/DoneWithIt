290640897380f427ad30ad27ec13183135e1bd40
Béla Jash <abelxotessema@gmail.com>
2025-01-11 10:16:31 +0300

    Button.

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
-import {StatusBar} from 'expo-status-bar';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight, TouchableNativeFeedback, View
-} from 'react-native';
+import {Button, SafeAreaView, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/*<Image source={require('./assets/icon.png')}/>*/}
-      {/*<TouchableWithoutFeedback onPress={() => console.log('tap')}>*/}
-      {/*<TouchableOpacity onPress={() => console.log('tap')}> /!* For some reason, it only works for double taps only. *!/*/}
-      <TouchableHighlight onPress={() => console.log('tap')}>
-        <Image
-          blurRadius={5}
-          loadingIndicatorSource={require('./assets/favicon.png')}
-          fadeDuration={1000} // Only for Android
-          resizeMode="cover"
-          onLoadStart={() => console.log('start')}
-          onLoad={() => console.log('loaded')}
-          onLoadEnd={() => console.log('end')}
-          onError={() => console.log('error')}
-          source={{width: 200, height: 300, uri: 'https://picsum.photos/200/300',}}/>
-      </TouchableHighlight>
-      <TouchableNativeFeedback>
-        <View style={{width: 200, height: 70, backgroundColor: 'dodgerblue'}}/>
-      </TouchableNativeFeedback>
+      <Button
+        title='Click me'
+        onPress={() => console.log('Button tapped.')}
+        color='green'
+      />
     </SafeAreaView>
   );
 }
