e899c92b0be7625d65cdb3d655f78220ea3c1f77
Béla Jash <abelxotessema@gmail.com>
2025-01-11 09:26:19 +0300

    View and Text.

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log('Yahallo!!!');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text
+        numberOfLines={1}
+        onPress={handlePress}
+      >
+        Open up App.js to start working on your app! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Alias deleniti ex exercitationem labore pariatur vel. Aliquid deleniti natus praesentium
+        voluptates?
+      </Text>
+      <StatusBar style="auto"/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
   },
 });
