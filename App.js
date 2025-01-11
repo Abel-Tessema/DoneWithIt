d24dd2fd108f9d4359fc324bd8f53b7b50892ee1
Béla Jash <abelxotessema@gmail.com>
2025-01-11 09:49:49 +0300

    Image.

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import {StatusBar} from 'expo-status-bar';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, Image} from 'react-native';
 
 export default function App() {
-  const handlePress = () => console.log('Yahallo!!!');
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text
-        numberOfLines={1}
-        onPress={handlePress}
-      >
-        Open up App.js to start working on your app! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Alias deleniti ex exercitationem labore pariatur vel. Aliquid deleniti natus praesentium
-        voluptates?
-      </Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      {/*<Image source={require('./assets/icon.png')}/>*/}
+      <Image
+        blurRadius={5}
+        loadingIndicatorSource={require('./assets/favicon.png')}
+        fadeDuration={1000} // Only for Android
+        resizeMode="cover"
+        onLoadStart={() => console.log('start')}
+        onLoad={() => console.log('loaded')}
+        onLoadEnd={() => console.log('end')}
+        onError={() => console.log('error')}
+        source={{width: 200, height: 300, uri: 'https://picsum.photos/200/300',}}/>
       <StatusBar style="auto"/>
     </SafeAreaView>
   );
@@ -23,5 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'dodgerblue',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
