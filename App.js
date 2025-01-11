25516ee70f16b4aaa94fce02f88bf0b734fac4c1
Béla Jash <abelxotessema@gmail.com>
2025-01-11 10:24:07 +0300

    Alert.

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import {Button, SafeAreaView, StyleSheet} from 'react-native';
+import {Alert, Button, SafeAreaView, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Button
         title='Click me'
-        onPress={() => console.log('Button tapped.')}
+        // onPress={() => alert('Button clicked')}
+        // onPress={() => Alert.alert(
+        //   'Custom Title',
+        //   'Custom message',
+        //   [
+        //     {text: 'Yes', onPress: () => console.log('Yes')},
+        //     {text: 'No', onPress: () => console.log('No')},
+        //   ]
+        // )}
+        onPress={() => Alert.prompt( // May only work on iOS.
+          'Custom Title',
+          'Custom message',
+          text => console.log(text),
+        )}
         color='green'
       />
     </SafeAreaView>
