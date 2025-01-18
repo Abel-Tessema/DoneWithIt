099d405908ba65252c7ae32c66d83271599af9c7
Béla Jash <abelxotessema@gmail.com>
2025-01-18 19:37:56 +0300

    Flexbox: flexBasis, flexGrow, and flexShrink.

--- App.js
+++ App.js
@@ -11,14 +11,28 @@ export default function App() {
         justifyContent: 'center',
         alignItems: 'center', // Aligns items on each line.
         alignContent: 'center', // Aligns the container itself as a whole.
-        flexWrap: 'wrap',
       }
     }).container}>
       <View style={StyleSheet.create({
         container: {
           backgroundColor: "dodgerblue",
-          width: 100,
-          height: 200,
+          width: 400,
+          height: 100,
+          // flexBasis
+          //    - Sets the size (width or height) in the direction of the main axis.
+          //    - Takes priority (over width or height) regardless of position in the code.
+          //    - Needs reload (on Expo) to take effect.
+          // flexBasis: 100,
+          // flexGrow
+          //    - Takes the available space on the main axis.
+          //    - Same as setting flex.
+          // flexGrow: 1,
+          // flex: 1,
+          // flexShrink
+          //    - Shrink to the available space so that all the others can fit on the line.
+          //    - Same as setting flex to a negative value.
+          // flexShrink: 1,
+          // flex: -1,
         }
       }).container}/>
       <View style={StyleSheet.create({
@@ -31,20 +45,6 @@ export default function App() {
       <View style={StyleSheet.create({
         container: {
           backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }
-      }).container}/>
-      <View style={StyleSheet.create({
-        container: {
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }
-      }).container}/>
-      <View style={StyleSheet.create({
-        container: {
-          backgroundColor: "greenyellow",
           width: 100,
           height: 100,
         }
