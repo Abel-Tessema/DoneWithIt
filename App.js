4f1acb230034e35c31993e2f52448f32f7f631a5
Béla Jash <abelxotessema@gmail.com>
2025-01-18 19:12:10 +0300

    Flexbox: justifyContent, alignItems, and alignSelf.

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
-import {Dimensions, Platform, SafeAreaView, StatusBar, StyleSheet, useWindowDimensions, View} from 'react-native';
-import {useDeviceOrientation} from "@react-native-community/hooks";
+import {View} from 'react-native';
+import {StyleSheet} from 'react-native';
 
 export default function App() {
-  // console.log(Dimensions.get('screen')); // Doesn't update with orientation
-  // console.log(useWindowDimensions()); // Updates with orientation
-  // console.log(useDeviceOrientation()); // Updates with orientation
-  const orientation = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        width: '100%',
-        height: orientation === 'landscape' ? '100%' : '30%',
-      }}>
-      </View>
-    </SafeAreaView>
+    <View style={StyleSheet.create({
+      container: {
+        backgroundColor: "white",
+        flex: 1, // The parent view has to be a flex container as well.
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }
+    }).container}>
+      <View style={StyleSheet.create({
+        container: {
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          alignSelf: 'flex-start',
+        }
+      }).container}/>
+      <View style={StyleSheet.create({
+        container: {
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }
+      }).container}/>
+      <View style={StyleSheet.create({
+        container: {
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }
+      }).container}/>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-});
